9d5dde7063d881dc3ce74b95b6b62511af878439
J.B.C <juanbicortelezzi@gmail.com>
2023-08-11 20:21:26 +1200

    add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const arr = [
+		['john', 'juan'],
+		['hello there!', 'buenas buenaaaas'],
+		['why cant I understand a thing?', 'porque no entiendo nada?']
+	];
+</script>
+
+<main class="min-h-screen flex justify-center items-center flex-col">
+	<ul>
+		{#each arr as [english, spanish]}
+			<li>item: {english} - {spanish}</li>
+		{/each}
+	</ul>
+	<button
+		class="border-2 border-black rounded-md px-4 py-2 hover:bg-black hover:text-white transition-colors ease-in-out"
+		>hello there</button
+	>
+</main>
